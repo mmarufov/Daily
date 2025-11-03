e4bf314620dccd80f9ff357c64fe695f7dab18dd
mmarufov <marufov254@gmail.com>
2025-11-03 12:42:27 -0800

    Add initial SwiftUI files for Daily app

---
+++ Daily/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Daily
+//
+//  Created by Muhammad on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
